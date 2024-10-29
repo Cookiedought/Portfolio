1ca0708e9b1aeea97be5c3f04fa21003f44cc628
Camille L <b.lemortcamille@gmail.com>
2024-10-29 16:44:40 +0100

    Projets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import avatar from "./assets/Avatar.png";
-import "./index.css"
-import Skills from "./components/Skills"
+import "./index.css";
+import Projets from "./components/Projets";
 
 function App() {
   return (
@@ -10,23 +10,23 @@ function App() {
           <ul className="link">
             <li>Accueil</li>
             <li>Compétences</li>
-            <li>Projets</li>
+            <li>Liens utiles</li>
             <li>Contact</li>
           </ul>
         </nav>
         <div className="presentation">
-          <img src={avatar}></img>
+          <img className="avatar" src={avatar}></img>
           <h1>Camille Lemort</h1>
-          <p className="skill">Developpeuse Web</p>
+          <p className="metier">Developpeuse Web</p>
         </div>
         <p className="scroll">Scroll Down</p>
       </header>
 
       <main>
-    <div>
-      <Skills/>
-    </div>
-        
+        <div className="skills">
+          <h2>Projets</h2>
+          <Projets />
+        </div>
       </main>
     </>
   );
---
+++ src/components/Projets.tsx
@@ -0,0 +1,10 @@
+function Projets() {
+  return (
+    <figure>
+      <img className="projet1" src="./assets/macato-eats"></img>
+      <figcaption>Projet 1</figcaption>
+    </figure>
+  );
+}
+
+export default Projets;
