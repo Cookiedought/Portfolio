03c9a17d433135a61570680e3317f34376907b0a
Camille L <b.lemortcamille@gmail.com>
2024-10-29 11:56:16 +0100

    Premier Composant

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-function App () {
-  return <div>Coucou c'est nous c'est derrick</div>
+import avatar from "./assets/Avatar.png";
+
+function App() {
+  return (
+    <section>
+      <nav>
+        <ul>
+          <li>Accueil</li>
+          <li>Compétences</li>
+          <li>Projets</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div>
+        <img src={avatar}></img>
+        <h1>Camille Lemort</h1>
+        <p>Developpeuse Web</p>
+      </div>
+      <p>Scroll Down</p>
+    </section>
+  );
 }
 
 export default App;
-
