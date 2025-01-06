628d2e4b8246dd549ee440b840d1f9fed79e9374
Camille L <b.lemortcamille@gmail.com>
2025-01-06 11:19:40 +0100

    Txt Presentation

--- src/components/Projets.tsx
+++ src/components/Projets.tsx
@@ -1,10 +1,13 @@
 function Projets() {
-  return (
-    <figure>
-      <img className="projet1" src="./public/macato-eats.png"></img>
-      <figcaption>Projet 1</figcaption>
-    </figure>
-  );
+    return (
+    <body>
+      <img className="MacatoEats" src="./public/macato-eats.png
+    "></img>
+    <img className="TravelUp" src="./public/TravelUp.png"></img>
+    </body>
+    )
 }
+
+
 
 export default Projets;
